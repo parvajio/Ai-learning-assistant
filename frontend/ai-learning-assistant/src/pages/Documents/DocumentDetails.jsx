f0059ff3076b20c2b7adb349e2a27007f8f1e318
parvajio <parvajio25@gmail.com>
2026-05-31 20:37:54 +0600

    documents pages

---
+++ frontend/ai-learning-assistant/src/pages/Documents/DocumentDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DocumentDetails = () => {
+  return (
+    <div>Document Details</div>
+  )
+}
+
+export default DocumentDetails
---
+++ frontend/ai-learning-assistant/src/pages/Documents/DocumentList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DocumentList = () => {
+  return (
+    <div>Document List</div>
+  )
+}
+
+export default DocumentList
